6083acb919522ba6cc5a7bfb1ce2f9f1bc80af10
lorderonnie <rontheking45@gmail.com>
2019-10-25 10:50:20 +0300

    final page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component } from '@angular/core'
+import { Goal } from './goal'
+import { from } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Goals';
+  // title = 'Goals';
+  // goals:string[];
+ 
+  // constructor(){
+  //   this.goals = ['Watch finding Nemo', 'Buy cookies', 'Get new phone case']
+  // } 
+  
 }
